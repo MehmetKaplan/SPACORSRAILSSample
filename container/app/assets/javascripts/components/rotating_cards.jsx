914accf0841b784b4185d84db5e72dad052bf1d3
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2017-05-03 19:09:23 +0300

    Rails upgrade to 5.1

---
+++ container/app/assets/javascripts/components/rotating_cards.jsx
@@ -0,0 +1,69 @@
+class RotatingCards extends React.Component {
+  render() {
+    return (
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-10 col-sm-offset-1">
+						<div className="col-md-4 col-sm-6">
+        {
+          this.props.cards_as_array_of_json.map(function(p_card_as_json) {
+            return   <div className="card-container manual-flip" key={p_card_as_json.id}>
+								<div className="card">
+									<div className="front">
+										<div className="cover">
+											<img src={p_card_as_json.card_cover_image}/>
+										</div>
+										<div className="user">
+											<img className="img-circle" src={p_card_as_json.card_circle_image}/>
+										</div>
+										<div className="content">
+											<div className="main">
+												<h3 className="name">{p_card_as_json.card_name}</h3>
+												<p className="profession">{p_card_as_json.card_title}</p>
+												<p className="text-center">{p_card_as_json.card_self_description}</p>
+											</div>
+											<div className="footer">
+												<button className="btn btn-simple" onClick={rotateCard(this)}>
+													<i className="fa fa-mail-forward"></i> {p_card_as_json.card_encourage_to_check_content}
+												</button>
+											</div>
+										</div>
+									</div>
+									<div className="back">
+										<div className="header">
+											<h5 className="motto">{p_card_as_json.card_back_header}</h5>
+										</div>
+										<div className="content">
+											<div className="main">
+												<h4 className="text-center">{p_card_as_json.card_back_title}</h4>
+												<p className="text-center">{p_card_as_json.card_back_content}</p>
+												<div className="stats-container">
+													<div className="stats">
+														<h4>{p_card_as_json.card_back_info_head}</h4>
+														<p>{p_card_as_json.card_back_info_content}</p>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div className="footer">
+											<button className="btn btn-simple" rel="tooltip" title="Flip Card" onClick={rotateCard(this)}>
+												<i className="fa fa-reply"></i> {p_card_as_json.card_back_to_front}
+											</button>
+											<div className="social-links text-center">
+												<a href="http://facebook.com" className="facebook"><i className="fa fa-facebook fa-fw"></i></a>
+												<a href="http://goofgle.com" className="google"><i className="fa fa-google-plus fa-fw"></i></a>
+												<a href="http://twitter.com" className="twitter"><i className="fa fa-twitter fa-fw"></i></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>;
+          })
+		  }
+						</div>
+					</div>
+				</div>
+			</div>
+    );
+  }
+}
--- container/app/assets/javascripts/usagesample.js.jsx
+++ container/app/assets/javascripts/usagesample.js.jsx
@@ -19,7 +19,7 @@ window.onload = function() {
 				p_call_result_as_json['DummyParameter'] + ' ' + 
 				p_call_result_as_json['ServerTimestamp'];
 			document.getElementById("container").innerHTML += l_text_to_add;
-			ReactDOM.render(React.createElement(Container, {kaplancustomtext: "This text is coming from usagesample.js: " + l_text_to_add}), 
+			ReactDOM.render(React.createElement(Container, {kaplancustomtext: "This text is coming from usagesample.js via manual React component generation: " + l_text_to_add}), 
 				document.getElementById("containerreact1")
 			);
 
@@ -53,7 +53,7 @@ window.onload = function() {
 				p_call_result_as_json['DummyParameter'] + ' ' + 
 				p_call_result_as_json['ServerTimestamp'];
 			document.getElementById("container").innerHTML += l_text_to_add;
-			ReactDOM.render(React.createElement(Container, {kaplancustomtext: "This text is coming from usagesample.js: " + l_text_to_add}), 
+			ReactDOM.render(React.createElement(Container, {kaplancustomtext: "This text is coming from usagesample.js via manual React component generation: " + l_text_to_add}), 
 				document.getElementById("containerreact2")
 			);
 		};
@@ -66,7 +66,7 @@ window.onload = function() {
 				p_call_result_as_json['DummyParameter'] + ' ' + 
 				p_call_result_as_json['ServerTimestamp'];
 			document.getElementById("content").innerHTML += l_text_to_add;
-			ReactDOM.render(React.createElement(Content, {kaplancustomtext: "This text is coming from usagesample.js: " + l_text_to_add}), 
+			ReactDOM.render(React.createElement(Content, {kaplancustomtext: "This text is coming from usagesample.js via manual React component generation: " + l_text_to_add}), 
 				document.getElementById("contentreact1")
 			);
 		};
@@ -79,11 +79,43 @@ window.onload = function() {
 				p_call_result_as_json['DummyParameter'] + ' ' + 
 				p_call_result_as_json['ServerTimestamp'];
 			document.getElementById("content").innerHTML += l_text_to_add;
-			ReactDOM.render(React.createElement(Content, {kaplancustomtext: "This text is coming from usagesample.js: " + l_text_to_add}), 
+			ReactDOM.render(React.createElement(Content, {kaplancustomtext: "This text is coming from usagesample.js via manual React component generation: " + l_text_to_add}), 
 				document.getElementById("contentreact2")
 			);
 		};
 		fetch_data_generic(l_function_to_execute_with_result_json, 'http://localhost:3001/datapost', 'POST', l_params_as_json, 'SAME', '');
+
+		var l_cards_as_array_of_json = [
+													{
+														id: 1,
+														card_cover_image: 'http://news.mit.edu/sites/mit.edu.newsoffice/files/styles/browse_news_image/public/images/2017/MIT-Machine_Learning-1.jpg', 
+														card_circle_image: 'http://news.mit.edu/sites/mit.edu.newsoffice/files/styles/browse_news_image/public/images/2017/MIT-Machine_Learning-1.jpg', 
+														card_name: 'a', 
+														card_title: 'a', 
+														card_self_description: 'a', 
+														card_encourage_to_check_content: 'BACK', 
+														card_back_header: 'a', 
+														card_back_title: 'a', 
+														card_back_content: 'a', 
+														card_back_to_front: 'a'
+													}, 
+													{
+														id: 2,
+														card_cover_image: 'http://news.mit.edu/sites/mit.edu.newsoffice/files/styles/browse_news_image/public/images/2017/MIT-Machine_Learning-1.jpg', 
+														card_circle_image: 'http://news.mit.edu/sites/mit.edu.newsoffice/files/styles/browse_news_image/public/images/2017/MIT-Machine_Learning-1.jpg', 
+														card_name: 'a2', 
+														card_title: 'a2', 
+														card_self_description: 'a2', 
+														card_encourage_to_check_content: 'BACK - a2', 
+														card_back_header: 'a2', 
+														card_back_title: '2a', 
+														card_back_content: 'a2', 
+														card_back_to_front: 'a2'
+													}, 
+		];
+		ReactDOM.render(React.createElement(RotatingCards, {cards_as_array_of_json: l_cards_as_array_of_json}), 
+			document.getElementById("contentreact3")
+		);
 
 	}
 	catch(err) {
